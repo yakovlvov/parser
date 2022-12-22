a5fad793e0560346f1de6732f385b853b06ea507
yakovlvov <korollevov@gmail.com>
2022-12-22 12:53:53 +0400

    version22.12

--- main_parser.py
+++ main_parser.py
@@ -1,19 +1,20 @@
 import requests
 import time
 import csv
+from tqdm import tqdm, tqdm_gui, trange
 
 def take_100_users():
-    token = '#ADDYOURTOKEN'
+    token = input('Введите токен:')
     version = 5.92
-    domain = '#ADDNAMEOFGROUP'
+    domain = input('Введите ID сообщества:')
     count = 1000
     offset = 0
     all_users = []
     while offset < 100:
         response = requests.get('https://api.vk.com/method/groups.getMembers',
-                                params = {
+                                params={
                                     'access_token': token,
-                                    'v':version,
+                                    'v': version,
                                     'group_id': domain,
                                     'count': count,
                                     'offset': offset
@@ -21,14 +22,16 @@ def take_100_users():
         data = response.json()['response']['items']
         offset += 1
         all_users.extend(data)
-        time.sleep(0.5)
+    all_users = list(set(all_users))
     return all_users
-def users_file(all_users):
-    with open('users.csv','w') as file:
+def users_file(data):
+    name_file = input('Как назовем файл?:')
+    with open('%s.csv'%name_file, 'w') as file:
         a_pen = csv.writer(file)
-        a_pen.writerow('names')
-        for us in all_users:
-            a_pen.writerow(us)
+        a_pen.writerow(('ids', ))
+        for us in data:
+            a_pen.writerow(('vk.com/id'+str(us), ))
+    print('Файл готов')
 
 all_users = take_100_users()
 users_file(all_users)
